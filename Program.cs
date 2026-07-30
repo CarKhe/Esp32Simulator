8b49d418a96151d6cc646d5f84d3deaf7195587a
Carlos Rodriguez <carloskiller177@gmail.com>
2026-07-30 10:03:39 -0500

    Simulacion en el estado del cambio

--- Program.cs
+++ Program.cs
@@ -11,32 +11,49 @@ var options = new MqttClientOptionsBuilder()
     .Build();
 
 //Evento de escucha al recibir mensajes
-mqttClient.ApplicationMessageReceivedAsync += e =>
+mqttClient.ApplicationMessageReceivedAsync += async e =>
 {
-    var mensaje = e.ApplicationMessage.ConvertPayloadToString();
-    Console.WriteLine($"[Comando recibido] Topic: {e.ApplicationMessage.Topic} -> {mensaje}");
-    return Task.CompletedTask;
-};
+    var topic = e.ApplicationMessage.Topic; // rancho/reles/riego/12/cmd
+    var payload = e.ApplicationMessage.ConvertPayloadToString(); // on
+    Console.WriteLine($"[Comando recibido] Topic: {topic} -> {payload}");
 
+    var partes = topic.Split('/'); // ["rancho","reles","riego","12","cmd"]
+    var tipo = partes[2];
+    var id = partes[3];
+
+    //Console.WriteLine($"[Simulando hardware] {tipo} {id} ahora esta: {payload}");
+
+    var exito = ValidarCambio();
+
+    var estadoReal = exito ? payload : "sin_cambio"; // si falló, no se movió del estado anterior
+
+    var resultado = new { estado = estadoReal, exito };
+    var json = System.Text.Json.JsonSerializer.Serialize(resultado);
+
+    var estadoTopic = $"rancho/reles/{tipo}/{id}/estado";
+    var mensajeEstado = new MqttApplicationMessageBuilder()
+        .WithTopic(estadoTopic)
+        .WithPayload(json)
+        .WithRetainFlag()
+        .Build();
+
+    await mqttClient.PublishAsync(mensajeEstado);
+    Console.WriteLine($"[Publicado] {estadoTopic} -> {json}");
+};
 //Abre la conexión real con el broker.
 await mqttClient.ConnectAsync(options, CancellationToken.None);
 Console.WriteLine("Conectado al broker MQTT");
 
 //Le dice al broker "avísame cuando algo se publique en rancho/reles/cmd".
 var subscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
-    .WithTopicFilter(f => f.WithTopic("rancho/reles/+/cmd"))
+    .WithTopicFilter(f => f.WithTopic("rancho/reles/+/+/cmd"))
     .Build();
 
-var subscribeOptionsLuces = mqttFactory.CreateSubscribeOptionsBuilder()
-    .WithTopicFilter(f => f.WithTopic("rancho/luces/+/cmd"))
-    .Build();
 
 //quiero recibir todo lo que se publique en tal topic
 await mqttClient.SubscribeAsync(subscribeOptions, CancellationToken.None);
 Console.WriteLine("Suscrito a rancho/reles/cmd, esperando comandos...");
 
-await mqttClient.SubscribeAsync(subscribeOptionsLuces, CancellationToken.None);
-Console.WriteLine("Suscrito a rancho/luces/cmd, esperando comandos...");
 
 while (true)
 {
@@ -52,4 +69,11 @@ while (true)
     Console.WriteLine($"[Publicado] rancho/temp -> {temp}");
 
     await Task.Delay(10000);
+}
+
+
+bool ValidarCambio()
+{
+    // Simula que a veces falla, para poder probar el camino de error
+    return new Random().Next(100) > 10; // 90% exito, 10% falla simulada
 }
